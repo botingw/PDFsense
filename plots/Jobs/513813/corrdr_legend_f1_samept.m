bb3db53bd38ad9a258791e7272626fb525f1a41f
spatrickdoyle <sean@spatrickdoyle.com>
2018-01-20 13:17:57 -0600

    removed extraneous stuff

--- plots/Jobs/513813/corrdr_legend_f1_samept.m
+++
@@ -1,6 +0,0 @@
-(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
-Grid[{{Text[Style["Sensitivity to u(x,\[Mu]), CT14NNLO\n\n", 
-     FontSize -> 24]], SpanFromLeft}, {"Data Sets"}, 
-  {"\[FilledCircle]BcdF2pCor(101)"}, {Text[Style["", FontSize -> 16]], 
-   SpanFromLeft}}, ItemStyle -> Directive[FontSize -> 18, GrayLevel[0]], 
- ItemSize -> 14, Alignment -> Left]
